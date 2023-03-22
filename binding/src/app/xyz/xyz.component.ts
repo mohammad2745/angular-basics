f885679e365e8b1eec6450fd69b38d587464601f
mohammad2745 <mohammadfcc2745@gmail.com>
2023-03-22 19:09:28 +0600

    Property-Class-Style-Event Binding

--- binding/src/app/xyz/xyz.component.ts
+++ binding/src/app/xyz/xyz.component.ts
@@ -6,9 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./xyz.component.scss']
 })
 export class XyzComponent {
+  //Property Binding
   public name = "John";
   public myId = "testId";
+
+  //Class Binding
   public isDisabled = true;
+  public successClass = "text-success";
+  public isSpecial = true;
+  public hasError = true;
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
+
+  //Style Binding
+  public highlightColor = "orange";
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
+  }
+
+  //Event Binding
+  public greeting = "";
+
+  onClick(event: Event){
+    console.log("Welcome To Angular event binding.");
+    console.log(event);
+    
+    this.greeting = "Welcome To Angular event binding."
+  }
 
   greetUser() {
     return "Hello " + this.name;
